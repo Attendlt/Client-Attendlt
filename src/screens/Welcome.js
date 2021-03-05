b8e7e797fc73867989136b778c072e5f6aa5fedd
vimalgupta24 <vimar4bsa@gmail.com>
2021-03-05 09:31:59 +0000

    footer

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import * as routes from "../constants/routes";
 import Nav from '../components/header';
+import Down from '../components/footer';
+
 function Welcome() {
-  return (
+  return (<>
     <Nav/>
+    <Down/>
+    </>
   );
 }
 
